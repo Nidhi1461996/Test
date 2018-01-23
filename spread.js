d359e2f08503d9a2cdaedfbad69211d3b28e758c
Nidhi1461996 <nidhijune14@gmail.com>
2018-01-23 12:49:31 +0530

    Delete spread.js

--- spread.js
+++
@@ -1,9 +0,0 @@
-function max(numbers){
-  var min = Math.min(...numbers);
-  if(numbers.length===0){
-    return null;
-  }
-  return `The minimum of [${numbers}] is ${min}`;
-}
-
-module.exports=max;
