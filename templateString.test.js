a6f87b107971b283a9a9dde25e4e217c08af6bcb
Nidhi1461996 <nidhijune14@gmail.com>
2018-01-23 12:49:49 +0530

    Delete templateString.test.js

--- templateString.test.js
+++
@@ -1,6 +0,0 @@
-const tempString=require('./templateString');
-
-test('The string will evaluate to', function(){
-  expect(tempString('NIDHI')).toBe(`Hello, templateString.test.js!
-Your name lowercased is "templatestring.test.js"`);
-});
